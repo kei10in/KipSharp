40f50b27a40bb79a09d046d23a934cddd48966d0
kei10in <kei10in@gmail.com>
2015-10-27 13:15:25 +0900

    Add PrintSchema Keywords

---
+++ Kip/PrintSchemaKeywords.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+
+namespace Kip
+{
+    public static class PrintSchemaKeywords
+    {
+        public static readonly XNamespace Url = "http://schemas.microsoft.com/windows/2003/08/printing/printschemakeywords";
+
+        public static readonly XName PickOne = Url + "PickOne";
+        public static readonly XName PickMany = Url + "PickMany";
+        public static readonly XName DisplayUI = Url + "DisplayUI";
+        public static readonly XName DisplayName = Url + "DisplayName";
+    }
+}
