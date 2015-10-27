875402cf4ee0c5df748a0d228d02e00b2968e87b
kei10in <kei10in@gmail.com>
2015-10-27 14:03:15 +0900

    Add PrintSchema Framework names

--- Kip/PrintSchemaFramework.cs
+++ Kip/PrintSchemaFramework.cs
@@ -7,8 +7,12 @@ namespace Kip
         public static readonly XNamespace Url = "http://schemas.microsoft.com/windows/2003/08/printing/printschemaframework";
 
         public static readonly XName PrintCapabilities = Url + "PrintCapabilities";
+        public static readonly XName PrintTicket = Url + "PrintTicket";
         public static readonly XName Feature = Url + "Feature";
         public static readonly XName Option = Url + "Option";
+        public static readonly XName ParameterDef = Url + "ParameterDef";
+        public static readonly XName ParameterInit = Url + "ParameterInit";
+        public static readonly XName ParameterRef = Url + "ParameterRef";
         public static readonly XName Property = Url + "Property";
         public static readonly XName ScoredProperty = Url + "ScoredProperty";
         public static readonly XName Value = Url + "Value";
