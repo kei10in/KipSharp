914fcf8db8dae8d338638635f90fc8c8dca84629
kei10in <kei10in@gmail.com>
2015-11-06 22:55:06 +0900

    Add DuplicationNameException

--- Kip/Exceptions.cs
+++ Kip/Exceptions.cs
@@ -6,37 +6,28 @@ using System.Threading.Tasks;
 
 namespace Kip
 {
+    /// <summary>
+    /// Thrown when an attempt is made to add a Print Schema element with the
+    /// name that already exists to the parent element.
+    /// </summary>
+    public class DuplicateNameException : InvalidOperationException
+    {
+        public DuplicateNameException() { }
+        public DuplicateNameException(string message) : base(message) { }
+        public DuplicateNameException(string message, Exception inner) : base(message, inner) { }
+    }
+
     public class InvalidChildElementException : Exception
     {
-        public InvalidChildElementException()
-        {
-        }
-
-        public InvalidChildElementException(string message)
-            : base(message)
-        {
-        }
-
-        public InvalidChildElementException(string message, Exception inner)
-            : base(message, inner)
-        {
-        }
+        public InvalidChildElementException() { }
+        public InvalidChildElementException(string message) : base(message) { }
+        public InvalidChildElementException(string message, Exception inner) : base(message, inner) { }
     }
 
     public class ReadPrintSchemaDocumentException : Exception
     {
-        public ReadPrintSchemaDocumentException()
-        {
-        }
-
-        public ReadPrintSchemaDocumentException(string message)
-            : base(message)
-        {
-        }
-
-        public ReadPrintSchemaDocumentException(string message, Exception inner)
-            : base(message, inner)
-        {
-        }
+        public ReadPrintSchemaDocumentException() { }
+        public ReadPrintSchemaDocumentException(string message) : base(message) { }
+        public ReadPrintSchemaDocumentException(string message, Exception inner) : base(message, inner) { }
     }
 }
