50173754c5de997e3a71cdec268faccebe9df89a
kei10in <kei10in@gmail.com>
2015-11-10 21:55:15 +0900

    Add keywords of the Print Schema Keywords v1.1

---
+++ Kip/PrintSchemaKeywordsV11.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Linq;
+
+namespace Kip
+{
+    public static class Pskv11
+    {
+        public static readonly XNamespace Namespace = "http://schemas.microsoft.com/windows/2013/05/printing/printschemakeywordsv11";
+
+        public static readonly XName JobPasscode = Namespace + "JobPasscode";
+        public static readonly XName JobPasscodeString = Namespace + "JobPasscodeString";
+        public static readonly XName JobImageFormat = Namespace + "JobImageFormat";
+        public static readonly XName Jpeg = Namespace + "Jpeg";
+        public static readonly XName PNG = Namespace + "PNG";
+    }
+}
