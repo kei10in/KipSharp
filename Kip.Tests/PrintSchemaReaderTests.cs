21daf36f558087733eb4e55053d5f22e5ec9b47b
kei10in <kei10in@gmail.com>
2015-11-14 11:29:59 +0900

    :art: Organize tests

--- Kip.Tests/PrintSchemaReaderTests.cs
+++ Kip.Tests/PrintSchemaReaderTests.cs
@@ -4,45 +4,68 @@ using System.Text;
 using System.Xml;
 using System.Xml.Linq;
 using Xunit;
+using static Kip.Tests.Utility;
 
 namespace Kip.Tests
 {
     public class PrintSchemaReaderTests
     {
-
         [Fact]
         public void ReadSimpleFeatureOptionStructure()
         {
-            Assembly assembly = this.GetType().GetTypeInfo().Assembly;
-            var names = assembly.GetManifestResourceNames();
-            using (var stream = assembly.GetManifestResourceStream("Kip.Tests.Data.SimpleFeatureOptionStructure.xml"))
-            {
-                var actual = Capabilities.Load(stream);
+            var pc = CreatePrintCapabilities(@"
+                <psf:Feature name='psk:JobCollateAllDocuments'>
+                  <psf:Property name='psf:SelectionType'>
+                    <psf:Value xsi:type='xsd:QName'>psk:PickOne</psf:Value>
+                  </psf:Property>
+                  <psf:Property name='psk:DisplayName'>
+                    <psf:Value xsi:type='xsd:string'>Collate Copies</psf:Value>
+                  </psf:Property>
+                  <psf:Option name='psk:Collated' constrained='psk:None'>
+                    <psf:Property name='psk:DisplayName'>
+                      <psf:Value xsi:type='xsd:string'>Yes</psf:Value>
+                    </psf:Property>
+                  </psf:Option>
+                  <psf:Option name='psk:Uncollated' constrained='psk:None'>
+                    <psf:Property name='psk:DisplayName'>
+                      <psf:Value xsi:type='xsd:string'>No</psf:Value>
+                    </psf:Property>
+                  </psf:Option>
+                </psf:Feature>");
 
-                Assert.NotNull(actual);
-                Assert.True(0 < actual.Features().Count());
+            var actual = Capabilities.Parse(pc);
 
-                var f = actual.Feature(Psk.JobCollateAllDocuments);
-                Assert.True(0 < f?.Options()?.Count());
+            Assert.NotNull(actual);
+            Assert.True(0 < actual.Features().Count());
 
-                var d = f.Property(Psk.DisplayName);
-                Assert.Equal("Collate Copies", d?.Value);
+            var f = actual.Feature(Psk.JobCollateAllDocuments);
+            Assert.True(0 < f?.Options()?.Count());
 
-                var o = f.Options().Select(x => x.Property(Psk.DisplayName)?.Value);
-                Assert.Contains(o, x => x == "Yes");
-                Assert.Contains(o, x => x == "No");
-            }
+            var d = f.Property(Psk.DisplayName);
+            Assert.Equal("Collate Copies", d?.Value);
+
+            var o = f.Options().Select(x => x.Property(Psk.DisplayName)?.Value);
+            Assert.Contains(o, x => x == "Yes");
+            Assert.Contains(o, x => x == "No");
         }
 
         [Fact]
         public void ReadValueWithUnspecifedType()
         {
-            Assembly assembly = this.GetType().GetTypeInfo().Assembly;
-            var names = assembly.GetManifestResourceNames();
-            using (var stream = assembly.GetManifestResourceStream("Kip.Tests.Data.ValueWithUnspecifiedType.xml"))
-            {
-                var actual = Capabilities.Load(stream);
-            }
+            var pc = CreatePrintCapabilities(@"
+                <psf:Feature name='psk:JobCollateAllDocuments'>
+                  <psf:Property name='psk:DisplayName'>
+                    <psf:Value>Copies Collate</psf:Value>
+                  </psf:Property>
+                  <psf:Option name='psk:Collated'></psf:Option>
+                  <psf:Option name='psk:Uncollated'></psf:Option>
+                </psf:Feature>");
+
+            var actual = Capabilities.Parse(pc);
+            var value = actual.Feature(Psk.JobCollateAllDocuments)?.Property(Psk.DisplayName)?.Value;
+            Assert.NotNull(value);
+            Assert.Equal(Xsd.String, value?.ValueType);
+            Assert.Equal("Copies Collate", value?.AsString());
         }
     }
 
---
+++ Kip.Tests/Utility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Kip.Tests
+{
+    internal static class Utility
+    {
+        private static readonly string PrintCapabilitiesHeader =
+            @"<?xml version='1.0' encoding='UTF-8'?>
+            <psf:PrintCapabilities
+                version='1'
+                xmlns:psf='http://schemas.microsoft.com/windows/2003/08/printing/printschemaframework'
+                xmlns:psk='http://schemas.microsoft.com/windows/2003/08/printing/printschemakeywords'
+                xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
+                xmlns:xsd='http://www.w3.org/2001/XMLSchema'>";
+        private static readonly string PrintCapabilitiesFooter = "</psf:PrintCapabilities>";
+
+        internal static string CreatePrintCapabilities(string content)
+        {
+            return string.Join(
+                Environment.NewLine,
+                PrintCapabilitiesHeader,
+                content,
+                PrintCapabilitiesFooter);
+        }
+    }
+}
