6dfdbc86ca1cec327719614fe26cf63882ee293b
kei10in <kei10in@gmail.com>
2015-12-06 15:46:56 +0900

    :white_check_mark: Add test for ParameterDef

---
+++ Kip.Tests/ParameterDefTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Kip.Tests
+{
+    public class ParameterDefTests
+    {
+        private List<Tuple<ParameterDef, ParameterDef>> _equalsPair = new List<Tuple<ParameterDef, ParameterDef>>()
+        {
+            Tuple.Create<ParameterDef, ParameterDef>(null, null),
+            Tuple.Create(new ParameterDef("a"), new ParameterDef("a")),
+            Tuple.Create(new ParameterDef("a", new Property("b")), new ParameterDef("a", new Property("b"))),
+        };
+
+        [Fact]
+        public void EqualsOpeartorAndTrue()
+        {
+            foreach (var pair in _equalsPair)
+            {
+                Assert.Equal(pair.Item1, pair.Item2);
+            }
+        }
+
+        private List<Tuple<ParameterDef, ParameterDef>> _notEqualsPair = new List<Tuple<ParameterDef, ParameterDef>>()
+        {
+            Tuple.Create<ParameterDef, ParameterDef>(null, new ParameterDef("a")),
+            Tuple.Create<ParameterDef, ParameterDef>(new ParameterDef("a"), null),
+            Tuple.Create(new ParameterDef("a"), new ParameterDef("b")),
+            Tuple.Create(new ParameterDef("a"), new ParameterDef("a", new Property("b"))),
+            Tuple.Create(new ParameterDef("a", new Property("b")), new ParameterDef("a", new Property("c"))),
+        };
+
+        [Fact]
+        public void EqualsOpeartorAndFalse()
+        {
+            foreach (var pair in _notEqualsPair)
+            {
+                Assert.NotEqual(pair.Item1, pair.Item2);
+            }
+        }
+    }
+}
